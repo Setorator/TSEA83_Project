a930018893acb9650c40da0b76ede761843f3299
Setorator <kimlarsson@comhem.se>
2016-04-14 16:34:40 +0200

    Rename cpu.vhd to CPU.vhd

--- CPU.vhd
+++ CPU.vhd
@@ -1,8 +1,8 @@
--- Detta ‰r en CPU av Olle roos typ med avbrott
--- Alla register fˆrrutom DR och IR ‰r 12 bitar breda, DR och IR ‰r 19
--- Program minnet ‰r 19 bitar brett
--- Bussen ‰r 19 bitar brett
--- Mikrominnet ‰r 33 bitar brett
+-- Detta √§r en CPU av Olle roos typ med avbrott
+-- Alla register f√∂rrutom DR och IR √§r 12 bitar breda, DR och IR √§r 19
+-- Program minnet √§r 19 bitar brett
+-- Bussen √§r 19 bitar brett
+-- Mikrominnet √§r 33 bitar brett
 
 
 library IEEE;
@@ -27,7 +27,7 @@ architecture Behavioral of cpu is
 
   -- uM = ALU_TB_FB_PC_I_SEQ_RW_SP_uAddr
   
-  -- Skriv mikrominne nedanfˆr
+  -- Skriv mikrominne nedanf√∂r
   constant u_mem_c : u_mem_t := (others => (others => '0'));
 
   signal u_mem : u_mem_t := u_mem_c;
@@ -46,18 +46,18 @@ architecture Behavioral of cpu is
   signal SPsig : unsigned(1 downto 0);         -- Manipulera stackpekaren
   signal PCsig : std_logic;                    -- PC++
 
-  -- Nollst‰ll uPC
+  -- Nollst√§ll uPC
 
   signal uPCzero : std_logic;
 
   -- uPC++
   signal uPCsig : std_logic;
 
-  -- L‰s K1
+  -- L√§s K1
 
   signal K1sig : std_logic;
 
-  -- L‰s K2
+  -- L√§s K2
 
   signal K2sig : std_logic;
 
@@ -81,30 +81,30 @@ architecture Behavioral of cpu is
   
   type p_mem_t is array(0 to 4095) of unsigned(18 downto 0);
 
-  -- Skriv program minne nedanfˆr
+  -- Skriv program minne nedanf√∂r
   
   constant p_mem_c : p_mem_t :=  (others => (others => '0'));           
 
-  signal p_mem : p_mem_t := p_mem_c;         -- S‰tt program minne
+  signal p_mem : p_mem_t := p_mem_c;         -- S√§tt program minne
 
   signal DR     : unsigned(18 downto 0);     -- Dataregister
   signal ADR    : unsigned(11 downto 0);     -- Address register
-  signal PC     : unsigned(11 downto 0);     -- Program r‰knaren
+  signal PC     : unsigned(11 downto 0);     -- Program r√§knaren
   signal IR     : unsigned(18 downto 0);     -- Instruktion register
   signal XR     : unsigned(11 downto 0);     -- XR
   signal SP     : unsigned(11 downto 0);     -- Stack pekare
-  signal TR     : unsigned(11 downto 0);     -- Tempor‰ra register
+  signal TR     : unsigned(11 downto 0);     -- Tempor√§ra register
   signal AR     : unsigned(11 downto 0);     -- Ackumulator register
   signal SR     : unsigned(11 downto 0);     -- Status register
-  signal HR     : unsigned(11 downto 0);     -- Hj‰lp register
+  signal HR     : unsigned(11 downto 0);     -- Hj√§lp register
   signal DATA_BUSS : unsigned(18 downto 0);   -- Bussen 2 byte
   
   begin
-    -- S‰tt mikrosignalen
+    -- S√§tt mikrosignalen
     
     uM <= u_mem(to_integer(uPC));
     
-    -- Kombinatorik fˆr avl‰sning uM
+    -- Kombinatorik f√∂r avl√§sning uM
     
     uAddr <= uM(7 downto 0);
     SPsig <= uM(9 downto 8);
@@ -116,7 +116,7 @@ architecture Behavioral of cpu is
     TB <= uM(28 downto 23);
     ALU <= uM(32 downto 29);
 
-    -- S‰tt skifta l‰ge pÂ I
+    -- S√§tt skifta l√§ge p√• I
     
     Vippa_I : process(clk)
     begin
@@ -129,11 +129,11 @@ architecture Behavioral of cpu is
       end if;
     end process;
       
-    -- Kombinatorik fˆr K4_intr
+    -- Kombinatorik f√∂r K4_intr
 
     K4_intr <= (not I) and intr;
 
-    -- Kombinatorik fˆr K4
+    -- Kombinatorik f√∂r K4
 
     K4_input(0) <= K1sig;
     K4_input(1) <= K2sig;
