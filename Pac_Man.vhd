ffb6a28bb21022e84402bfdbdc72eeb72b0e6667
Setorator <kimlarsson@comhem.se>
2016-04-18 16:54:53 +0200

    Added files via upload
    Arbetat allmänt med projektet

--- Pac_Man.vhd
+++ Pac_Man.vhd
@@ -25,6 +25,7 @@ end Pac_Man;
 -- architecture
 architecture Behavioral of Pac_Man is
 
+
 	component CPU
 		port (
 			clk						  : in std_logic;								-- System clock
@@ -37,8 +38,9 @@ architecture Behavioral of Pac_Man is
 	component PIX_GEN
 		port (
 			clk                    : in std_logic;                      -- System clock
-			clk25                  : in std_logic;                      -- 25 MHz
 			rst                    : in std_logic;                      -- reset button
+			data	 					  : in std_logic_vector(7 downto 0);	-- Data to be read from MEM
+	 		addr						  : out std_logic_vector(10 downto 0);			-- Adress to the tile in MEM
 			Hsync                  : out std_logic;                     -- horizontal sync
 			Vsync                  : out std_logic;                     -- vertical sync
 			vgaRed                 : out std_logic_vector(2 downto 0);  -- VGA red
@@ -46,20 +48,44 @@ architecture Behavioral of Pac_Man is
 			vgaBlue                : out std_logic_vector(2 downto 1);  -- VGA blue
 			colision					  : out std_logic								-- Interupt 
 		);
-  end component;
+  	end component;
+  
+  
+	component RAM
+		port (
+			clk							: in std_logic;							-- System clock
 
-  signal ClkDiv				: unsigned(1 downto 0);						-- Clock divisor, to generate
-                                                 						-- 25 MHz clock
-  signal Clk25					: std_logic;		 							-- One pulse width 25 MHz sign
+			-- port 1
+			x1 							: in std_logic_vector(5 downto 0);	-- 64 columns, only 40 is used
+			y1 							: in std_logic_vector(4 downto 0);	-- 32 rows, only 30 used
+			rw1 							: in std_logic;							-- READ/WRITE
+			ce1							: in std_logic;							-- Count enable
+			data1_in						: in std_logic_vector(7 downto 0);	-- Data to be written
+			data1_out					: out std_logic_vector(7 downto 0);	-- Data to be read
+			-- port 2
+			x2 							: in std_logic_vector(5 downto 0);	-- 64 columns, only 40 is used
+			y2 							: in std_logic_vector(4 downto 0);	-- 32 rows, only 30 used
+			rw2 							: in std_logic;							-- READ/WRITE
+			ce2							: in std_logic;							-- Count enable
+			data2							: out std_logic_vector(7 downto 0)-- Data to be read/written		
+		);
+	end component;
+		
   
   signal interupt				: std_logic;									-- Signal between CPU and PIX_GEN
+  
+  signal addr_tmp				: std_logic_vector(10 downto 0);
+  signal data_tmp				: std_logic_vector(7 downto 0);
 
 begin 
 
 	U0 : CPU port map(clk=>clock, rst=>reset, intr=>interupt);
+	U1 : RAM port map(clk=>clock, ce1=>'1', rw1=>'0', data1_in=>"00000000", x1=>"000000", y1=>"00000", 
+							ce2=>'0', rw2=>'1', data2=>data_tmp, x2=>addr_tmp(5 downto 0), y2=>addr_tmp(10 downto 6));
 
-	U1 : PIX_GEN port map(clk=>clock, clk25=>Clk25, rst=>reset, 
+	U2 : PIX_GEN port map(clk=>clock, rst=>reset, 
 									Hsync=>horizontalSync, Vsync=>verticalSync,
+									data=>data_tmp, addr=>addr_tmp,
 									vgaRed(2)=>video(7),
 									vgaRed(1)=>video(6),
 									vgaRed(0)=>video(5),
@@ -70,21 +96,8 @@ begin
 									vgaBlue(1)=>video(0),
 									colision=>interupt);
 
-  
-  -- Clock divisor
-  -- divide system clock (100 MHz) by 4
-  Clk_div : process(clock)
-  begin
-    if rising_edge(clock) then
-      if reset='1' then
-	ClkDiv <= (others => '0');
-      else
-	ClkDiv <= ClkDiv + 1;
-      end if;
-    end if;
-  end process;
-  -- 25 MHz clock (one system clock pulse width)
-  Clk25 <= '1' when (ClkDiv = 3) else '0';
 
   
 end Behavioral;
+
+
